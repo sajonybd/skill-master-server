3d027807fca1eccd67cf2defebeb06d475b2dee3
Safiul Alom <sajonybd@gmail.com>
2022-10-27 09:57:51 +0600

    some bug fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const courses = require('./api/courses.json');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,18 @@ app.use(cors());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Skill Master API Server v1 Running!')
+  res.send('Skill Master API Server Running!')
+})
+
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const course = courses.find(cr => cr.id === id) || {};
+  console.log(course);
+  res.send(course)
 })
 
 app.listen(port, () => {
