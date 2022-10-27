ae7a7e7a86bac0262532e80c618c7c50680a2ed8
Safiul Alom <sajonybd@gmail.com>
2022-10-27 09:09:43 +0600

    api system added

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
+const courses = require('./api/courses.json');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Hello World Buddy!')
+})
+
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const course = courses.find(cr => cr.id === id) || {};
+  res.send(course)
 })
 
 app.listen(port, () => {
